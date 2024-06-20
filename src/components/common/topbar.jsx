88f12d3aa220301154d8d47a5808f9df58dd171a
tiger <44923556+eeeeaa@users.noreply.github.com>
2024-06-20 20:29:55 +0700

    add icon to topbar menu

--- src/components/common/topbar.jsx
+++ src/components/common/topbar.jsx
@@ -1,16 +1,21 @@
 import styles from "../../styles/common/topbar.module.css";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import { IoHomeOutline } from "react-icons/io5";
+import { BiWorld } from "react-icons/bi";
 
 MenuItem.propTypes = {
   link: PropTypes.string,
   title: PropTypes.string,
 };
 
-function MenuItem({ link, title }) {
+function MenuItem({ link, title, icon }) {
   return (
     <Link to={link} className={styles["menu-item"]}>
-      <li>{title}</li>
+      <li className={styles.item}>
+        {icon}
+        <p>{title}</p>
+      </li>
     </Link>
   );
 }
@@ -20,8 +25,8 @@ export default function TopBar() {
     <div className={styles["topbar"]}>
       <div className={styles["topbar-title"]}>MouseHole</div>
       <ul className={styles["menu-list"]}>
-        <MenuItem link={"/"} title={"Home"} />
-        <MenuItem link={"/all"} title={"All"} />
+        <MenuItem link={"/"} title={"Home"} icon={<IoHomeOutline />} />
+        <MenuItem link={"/all"} title={"All"} icon={<BiWorld />} />
       </ul>
     </div>
   );
